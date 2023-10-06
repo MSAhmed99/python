bb495ed39f645c0ec31f969fb9ee73d3ff0df29a
MSAhmed99 <msa.shakeel97@gmail.com>
2023-10-06 20:04:52 +0530

    added while loop

--- pizza price.py
+++ pizza price.py
@@ -1,21 +1,43 @@
-print("Welcome to pizza deliveries!")
-size = input("what size pizza do you want? S, M, or L: ")
-add_pepperoni = input("Do you want pepperoni? Y or N? ")
-extra_cheese = input("Do you want cheese? Y or N? ")
-bill = 0
-if size == "S":
+print("Thank you for chosing pizza deliveries")
+
+# to select the size
+size = input("what size pizza do you want? S, M, or L: ").upper()  #the .upper coverts the s to S
+while size not in ["S", "M", "L"]:
+    print("Invalid choice. Please select S, M, or L.")
+    size = input("What size pizza do you want? S, M, or L: ").upper()
+
+
+add_pepperoni = input("Do you want pepperoni? Y or N? ").upper()
+while add_pepperoni not in ["Y", "N"]:
+    print("Invalid choice. Please select Y or N.")
+    add_pepperoni = input("Do you want pepperoni? Y or N? ").upper()
+  
+
+extra_cheese = input("Do you want cheese? Y or N? ").upper()
+while extra_cheese not in ["Y", "N"]:
+    print("Invalid choice. Please select Y or N.")
+    extra_cheese = input("Do you want extra cheese? Y or N? ").upper()
+
+
+bill = 0 
+
+if size in("S", "s"):
     bill += 15
-elif size == "M":
+
+elif size in("M", "m"):
     bill += 20
-else: 
+
+else:
     bill += 25
-    
-if add_pepperoni == "Y":
-    if size == "S":
+
+if add_pepperoni in("Y", "y"):
+    if size == 'S':
         bill += 2
-    else: 
-        bill += 3 
-if extra_cheese == "Y":
-    if size == "S":
-        bill += 1
-print(f"Your final bill is ${bill}")
+    else:
+        bill += 3
+
+
+if extra_cheese in("Y", "y"):
+    bill += 1
+
+print(f"The final bill is: ${bill}")
